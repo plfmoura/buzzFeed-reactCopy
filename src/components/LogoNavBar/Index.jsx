c54954b4882e1b51d23db3a5f5226377fdb65b5e
plfmoura <plfmoura96@gmail.com>
2022-12-18 16:39:25 -0300

    alter(logos)change src of logos

--- src/components/LogoNavBar/Index.jsx
+++ src/components/LogoNavBar/Index.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function LogoNavBar() {
   return (
     <div>
-      <img src={"public/BuzzFeed-Logo.png"} alt="Logo da BuzzFeed" width="15%" />
+      <img src={"/navBar/BuzzFeed-Logo.png"} alt="Logo da BuzzFeed" width="15%" />
     </div>
   )
 }
